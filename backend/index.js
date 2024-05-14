5db725aa341ff42a3bbdc2a7396089e38e2d871c
Dhruv Vara <dhruvvara3@gmail.com>
2024-05-14 10:14:06 +0530

    Database connection

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors")
+
+const PORT = process.env.PORT || 5000;
+
+const db = require("./config/database");
 
 
-app.get("/",(req,res)=>{
+dotenv.config({ path: "./.env" });
+app.use(express.json());
+db.connect();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
     res.send("hello");
 })
 
 
-app.listen("5000",()=>{
-    console.log("Server running");
+
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server Running at ${PORT}`);
 })
